17aaf0a219c35ce59b8a483d513097865c01a1cc
Simone <scimonscassa@outlook.it>
2024-02-14 20:33:21 +0100

    Change PasswordDto, add logic to password CRUD methods

--- src/main/java/it/unimol/pwmanager/models/dto/password/PasswordDto.java
+++ src/main/java/it/unimol/pwmanager/models/dto/password/PasswordDto.java
@@ -14,22 +14,37 @@ import java.time.Instant;
 @AllArgsConstructor
 public class PasswordDto {
     private Long id;
-    private String value;
-    private String description;
-    private Boolean completed;
+    private String password;
+    private String title;
+    private Boolean used;
+    private Boolean expired;
     private Instant createdOn;
     private Instant updatedOn;
     private Instant expiresOn;
 
+    private String websiteProfileId;
+    private String websiteProfileEmail;
+
+    private String groupId;
+    private String groupName;
+
+
 
     public PasswordDto(Password password) {
         this.id = password.getId();
-        this.value = password.getDescription();
-        this.description = password.getDescription();
-        this.completed = password.getCompleted();
+        this.password = password.getPassword();
+        this.title = password.getTitle();
+        this.used = password.getUsed();
+        this.expired = password.getExpired();
         this.createdOn = password.getCreatedOn();
         this.updatedOn = password.getUpdatedOn();
-        this.completedOn = password.getExpiresOn();
-        this.tagName = password.getGroup().getName();
+        this.expiresOn = password.getExpiresOn();
+
+        this.websiteProfileId = password.getWebsiteProfile().getId().toString();
+        this.websiteProfileEmail = password.getWebsiteProfile().getEmail();
+
+        this.groupId = password.getGroup().getId().toString();
+        this.groupName = password.getGroup().getName();
+        
     }
 }
--- src/main/java/it/unimol/pwmanager/services/PasswordService.java
+++ src/main/java/it/unimol/pwmanager/services/PasswordService.java
@@ -11,12 +11,9 @@ import it.unimol.pwmanager.models.request.group.EditGroupRequest;
 import it.unimol.pwmanager.models.request.password.EditPasswordRequest;
 import it.unimol.pwmanager.models.request.password.PatchPasswordGroup;
 import it.unimol.pwmanager.models.request.websiteprofile.EditWebsiteProfileRequest;
-import org.apache.commons.crypto.utils.AES;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -30,12 +27,6 @@ public class PasswordService {
     private int passwordIdCounter;
     private int doerIdCounter;
     private int tagIdCounter;
-
-    private final SecretKeySpec key = AES.newSecretKeySpec(getUTF8Bytes("1234567890123456"));
-    private final IvParameterSpec iv = new IvParameterSpec(getUTF8Bytes("1234567890123456"));
-    private final String transform = AES.CBC_PKCS5_PADDING;
-    private static final String TEXT_REGEX = "";
-    private static final String NAME_AND_SURNAME_REGEX = "";
     private ByteArrayOutputStream outputStream;
 
     @Autowired()
@@ -66,31 +57,83 @@ public class PasswordService {
         throw new RuntimeException("Not implemented yet. Remove me when you´re done :)");
     }
 
-    // to-do CRUD operations
-
     public PasswordDto readPassword(Long id) {
-
-        throw new RuntimeException("Not implemented yet. Remove me when you´re done :)");
+        return passwords.stream()
+                .filter(p -> p.getId().equals(id))
+                .map(PasswordDto::new)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Password not found"));
     }
 
     public List<PasswordShortInfoDto> readPassword() {
-
-        throw new RuntimeException("Not implemented yet. Remove me when you´re done :)");
+        return passwords.stream()
+                .map(PasswordShortInfoDto::new)
+                .toList();
     }
 
     public void deletePassword(Long id) {
-
-        throw new RuntimeException("Not implemented yet. Remove me when you´re done :)");
+        Password password = passwords.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Password not found"));
+        passwords.remove(password);
     }
 
     public void updatePassword(Long id, EditPasswordRequest editPasswordRequest) {
+        // check if password is at least 8 characters long and max 20 characters long,
+        // has at least one uppercase letter, one lowercase letter, one number and one
+        // special character
+        if (!editPasswordRequest
+                .getPassword()
+                .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$")) {
+            throw new RuntimeException(
+                    "Password must be at least 8 characters long and max 20 characters long, has at least one uppercase letter, one lowercase letter, one number and one special character");
+        }
+
+        // name can't be blank, must be max 50 characters long, çan't be blank cant only contain numbers or special letters
+        if (editPasswordRequest.getTitle().isBlank() || editPasswordRequest.getTitle().length() > 50
+                || !editPasswordRequest.getTitle().matches("^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$")) {
+            throw new RuntimeException("Name can't be blank, must be max 50 characters long, çan't be blank cant only contain numbers or special letters");
+        }
+
+        // find group id, if not exists throw exception
+        Group group = groups.stream()
+                .filter(g -> g.getId().equals(editPasswordRequest.getGroupId()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Group not found"));
+
+        // find website profile id, if not exists throw exception
+        WebsiteProfile websiteProfile = websiteProfiles.stream()
+                .filter(wp -> wp.getId().equals(editPasswordRequest.getWebsiteProfileId()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Website profile not found"));
+
+        // find password, then update and resave it
+        Password password = passwords.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Password not found"));
+
+        password.setTitle(editPasswordRequest.getTitle());
+        password.setPassword(editPasswordRequest.getPassword());
+        password.setGroup(group);
+        password.setWebsiteProfile(websiteProfile);
 
         throw new RuntimeException("Not implemented yet. Remove me when you´re done :)");
     }
 
     public void patchPasswordExpiredStatus(Long id) {
+        // throw exception if password not found
+        if (passwords.stream().noneMatch(p -> p.getId().equals(id))) {
+            throw new RuntimeException("Password not found");
+        }
 
-        throw new RuntimeException("Not implemented yet. Remove me when you´re done :)");
+        passwords.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .ifPresent(p -> p.setExpired(true));
+
+
     }
 
     public void patchPasswordGroup(Long id, PatchPasswordGroup patchPasswordGroup) {
